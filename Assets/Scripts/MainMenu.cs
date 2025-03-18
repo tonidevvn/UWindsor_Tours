c2be87082dee13b252d60dc97efb54563df3d7c0
Toni Pham <139079071+tonidevvn@users.noreply.github.com>
2025-03-18 01:24:05 -0400

    update sound tracks for main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,23 +4,31 @@ using UnityEngine.UIElements;
 
 public class MainMenu: MonoBehaviour
 {
+    AudioManager audioManager;
     private void Awake()
     {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
         root.Q<Button>("start-btn").clicked += () => StartGame();
         root.Q<Button>("settings-btn").clicked += () => OpenSettings();
-        root.Q<Button>("quit-btn").clicked += () => QuitGame(); 
+        root.Q<Button>("quit-btn").clicked += () => QuitGame();
+        root.Q<Button>("start-btn").RegisterCallback<MouseEnterEvent>(evt => audioManager.PlayMouseHover());
+        root.Q<Button>("settings-btn").RegisterCallback<MouseEnterEvent>(evt => audioManager.PlayMouseHover());
+        root.Q<Button>("quit-btn").RegisterCallback<MouseEnterEvent>(evt => audioManager.PlayMouseHover());
     }
 
     private void StartGame() {
+        audioManager.PlayMouseClick();
         Loader.Load("Game");
     }
 
     private void OpenSettings() {
+        audioManager.PlayMouseClick();
         Debug.Log("Settings opened!");
     }
 
     private void QuitGame() {
+        audioManager.PlayMouseClick();
         Debug.Log("Quit game!");
         // save any game data here
         #if UNITY_EDITOR
